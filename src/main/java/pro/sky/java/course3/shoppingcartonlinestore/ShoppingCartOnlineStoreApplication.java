76072d89d49b2dbc5bfe71c450a68be8497e38a6
EkaterinaShulga <shulga23@gmail.com>
2022-07-01 08:01:10 +0300

    первый коммит

---
+++ src/main/java/pro/sky/java/course3/shoppingcartonlinestore/ShoppingCartOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course3.shoppingcartonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartOnlineStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingCartOnlineStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course3/shoppingcartonlinestore/ShoppingCartOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course3.shoppingcartonlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartOnlineStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
